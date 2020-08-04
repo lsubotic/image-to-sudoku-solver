11e761179f0dc6a95bcad1ebcd50cb08794faa41
Lazar <lakizmaj2603@hotmail.com>
2020-08-04 19:32:12 +0200

    added constants

---
+++ constants.py
@@ -0,0 +1,10 @@
+"""
+CONSTANTS
+"""
+
+
+IMAGE_PATH = 'sudoku.jpg'  # picture of sudoku
+SQUARES_FOLDER = 'square_images'  # folder to save splitted squares to
+
+MODEL_FILE = 'syn_model.hdf5'
+LABELS = 'labels.dat'
